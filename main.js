4b35cb8eb5aec84d4ad0f3c6265862b554abc4ae
Josh Avanier <joshavanier@gmail.com>
2018-01-21 13:16:51 +0700

    Restructure

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 const {app, BrowserWindow, webFrame} = require('electron')
-const path = require('path')
-const url = require('url')
+// const path = require('path')
+// const url = require('url')
 
 if (process.argv[2] && process.argv[2] === 'dev') require('electron-reload')(__dirname)
 
 var win
 
 app.on('ready', function() {
-  win = new BrowserWindow({width: 300, height: 500, backgroundColor: '#f8f8f8', resizable:true, autoHideMenuBar: true, frame: false})
+  win = new BrowserWindow({
+    width: 300,
+    height: 500,
+    backgroundColor: '#0c0c0c',
+    resizable: false,
+    autoHideMenuBar: true,
+    frame: false,
+    icon: `${__dirname}/icon.ico`
+  })
 
-  win.loadURL(`file://${__dirname}/index.html`)
+  win.loadURL(`file://${__dirname}/src/index.html`)
 
   win.on('closed', () => {
     win = null
